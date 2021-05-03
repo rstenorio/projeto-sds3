d4a2a9bf6464da19fe227d820901a48750401de9
Rodrigo Tenorio <rodrigo.s.tenorio@gmail.com>
2021-05-03 14:41:41 +0200

    Project created

---
+++ backend/src/main/java/com/devsuperior/dsvvendas/DsvvendasApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsvvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsvvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsvvendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dsvvendas/DsvvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsvvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
