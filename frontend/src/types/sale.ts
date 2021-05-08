f4a424fc0b8ab6a13402e882377dc34bb1f07d8a
Rodrigo Tenorio <rodrigo.s.tenorio@gmail.com>
2021-05-08 17:05:28 +0200

    Pagination

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -2,23 +2,23 @@ import { Seller } from "./seller"
 
 export type Sale = {
 
-    id: number,
-    visited: number,
-    deals: number,
-    amount: number,
-    date: string,
+    id: number;
+    visited: number;
+    deals: number;
+    amount: number;
+    date: string;
     seller: Seller
 }
 
 export type SalePage = {
     content?: Sale[];
-    last: boolean,
-    totalElements: number,
-    totalPages: number,
-    size?: number,
-    number: number,
-    first: boolean,
-    numberOfElements?: number,
+    last: boolean;
+    totalElements: number;
+    totalPages: number;
+    size?: number;
+    number: number;
+    first: boolean;
+    numberOfElements?: number;
     empty?: boolean
 }
 
